fd3d524906a1592d1c8301fca2dbec32d7875cb8
Your Namemgit config --global user.email 'mariasol039@hotmail.comgit commit  git config --global user.email you@example.comgit commit -m 'Add new'git pushgit config --global user.email you@example.com  git config --global user.name Your <you@example.com>
2024-05-09 17:40:13 -0300

    service animation added

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Services.css";
 import cimg1 from "./card1.svg";
@@ -48,42 +50,9 @@ const Services = () => {
         style={{ marginBottom: "-65px" }}
       >
         <div className="container mx-auto d-flex flex-column align-items-center flex-lg-row justify-content-lg-center align-items-lg-stretch gap-5">
-          <div className="row justify-content-center ">
+          <div className="row justify-content-center">
             {cardsInfo.map((card, index) => (
-              <div className="col-md-4 mb-3" key={index}>
-                <div className="card text-center h-100">
-                  <div className="card-body d-flex flex-column justify-content-center">
-                    <img
-                      className="bg-primary text-light rounded-circle p-1 mb-3 mx-auto"
-                      src={card.icon}
-                      alt=""
-                      style={{
-                        width: "60px",
-                        height: "60px",
-                        strokeWidth: "1",
-                      }}
-                    />
-                    <h3 className="card-title fs-6 fw-semibold mb-3">
-                      {card.title}
-                    </h3>
-                    <p className="card-text fw-medium">{card.description}</p>
-                    <div>
-                      <a
-                        href="#learn-more"
-                        className="btn btn-secondary fw-medium"
-                        style={{
-                          "--bs-btn-font-size": "12px",
-                          "--bs-btn-padding-x": "24px",
-                          "--bs-btn-color": "rgba(0, 0, 0, 0.8)",
-                          "--bs-btn-bg": "transparent",
-                        }}
-                      >
-                        Learn more
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <ServiceCard key={index} card={card} />
             ))}
           </div>
         </div>
@@ -92,5 +61,56 @@ const Services = () => {
   );
 };
 
+const ServiceCard = ({ card }) => {
+  const controls = useAnimation();
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start({ opacity: 1, x: 0 });
+    } else {
+      controls.start({ opacity: 0, x: -100 });
+    }
+  }, [controls, inView]);
+
+  return (
+    <div className="col-md-4 mb-3">
+      <motion.div
+        className="card text-center h-100"
+        ref={ref}
+        initial={{ opacity: 0, x: -100 }}
+        animate={controls}
+        transition={{ duration: 0.5 }}
+      >
+        <div className="card-body d-flex flex-column justify-content-center">
+          <img
+            className="bg-primary text-light rounded-circle p-1 mb-3 mx-auto"
+            src={card.icon}
+            alt=""
+            style={{ width: "60px", height: "60px", strokeWidth: "1" }}
+          />
+          <h3 className="card-title fs-6 fw-semibold mb-3">{card.title}</h3>
+          <p className="card-text fw-medium">{card.description}</p>
+          <div>
+            <a
+              href="#learn-more"
+              className="btn btn-secondary fw-medium"
+              style={{
+                "--bs-btn-font-size": "12px",
+                "--bs-btn-padding-x": "24px",
+                "--bs-btn-color": "rgba(0, 0, 0, 0.8)",
+                "--bs-btn-bg": "transparent",
+              }}
+            >
+              Learn more
+            </a>
+          </div>
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+
 export default Services;
+
 
