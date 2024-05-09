2f08b2aa2ec1b7a4b463154b974e14bfbbac34d1
Your Namemgit config --global user.email 'mariasol039@hotmail.comgit commit  git config --global user.email you@example.comgit commit -m 'Add new'git pushgit config --global user.email you@example.com  git config --global user.name Your <you@example.com>
2024-05-09 18:06:42 -0300

    animations finished

--- src/components/clients/Clients.jsx
+++ src/components/clients/Clients.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Clients.css";
 import c2 from "./c2.svg";
@@ -7,41 +7,101 @@ import c4 from "./c4.svg";
 import c5 from "./c5.svg";
 import c1 from "./c1.svg";
 import img from "./img.png";
+import { motion } from "framer-motion";
 
 const Clients = () => {
   return (
     <div className="container-fluid bg-white  clients mx-auto">
-      <div className='d-flex flex-column justify-content-center py-5 align-items-center'>
-        <div className='client-title py-5 text-center'>
-          <h3 className='fs-6 fw-semibold'>CLIENTS WE WORKED WITH</h3>
+      <div className="d-flex flex-column justify-content-center py-5 align-items-center">
+        <div className="client-title py-5 text-center">
+          <h3 className="fs-6 fw-semibold">CLIENTS WE WORKED WITH</h3>
         </div>
-        <div className='clients-brand row d-flex flex-lg-row flex-md-column justify-content-center align-items-center'>
-          <img className='clients-brand-icon' alt='c' src={c1}></img>
-          <img className='clients-brand-icon' alt='c' src={c2}></img>
-          <img className='clients-brand-icon' alt='c' src={c3}></img>
-          <img className='clients-brand-icon' alt='c' src={c4}></img>
-          <img className='clients-brand-icon' alt='c' src={c5}></img>
+        <div className="clients-brand row d-flex flex-lg-row flex-md-column justify-content-center align-items-center">
+          <img className="clients-brand-icon" alt="c" src={c1}></img>
+          <img className="clients-brand-icon" alt="c" src={c2}></img>
+          <img className="clients-brand-icon" alt="c" src={c3}></img>
+          <img className="clients-brand-icon" alt="c" src={c4}></img>
+          <img className="clients-brand-icon" alt="c" src={c5}></img>
         </div>
 
         <div className="row client-card">
           <div className="col-sm-6 mb-3 mb-sm-0 border-opacity-10">
-            <div className="card text-start bg-primary position-relative ">
-              <div className="card-body">
-                <h5 className="card-title text-start text-white fs-4 fw-semibold">Get in touch with us</h5>
-                <p className="card-text text-start text-white fs-6 w-50 py-2" style={{  opacity: 0.7 }}>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-                <a href="#" className="btn text-white border-white btn-outline-secondary start-0">Hire us</a>
+            <motion.div
+              initial={{ opacity: 0, x: -30, y: -50 }}
+              whileInView={{ opacity: 1, x: 5, y: 2 }}
+              transition={{
+                default: {
+                  type: "spring",
+                  damping: 10,
+                  duration: 2,
+                  stiffness: 40,
+                  ease: [0.5, 0.71, 0.2, 1.5],
+                },
+              }}
+            >
+              <div className="card text-start bg-primary position-relative ">
+                <div className="card-body">
+                  <h5 className="card-title text-start text-white fs-4 fw-semibold">
+                    Get in touch with us
+                  </h5>
+                  <p
+                    className="card-text text-start text-white fs-6 w-50 py-2"
+                    style={{ opacity: 0.7 }}
+                  >
+                    Maecenas sed diam eget risus varius blandit sit amet non
+                    magna.
+                  </p>
+                  <a
+                    href="#"
+                    className="btn text-white border-white btn-outline-secondary start-0"
+                  >
+                    Hire us
+                  </a>
+                </div>
               </div>
-            </div>
+            </motion.div>
           </div>
           <div className="col-sm-6 ">
-            <div className="card border-opacity-10 text-start bg-primary position-relative">
-              <div className="card-body text-white">
-                <h5 className="card-title text-white text-start fs-4 fw-semibold">Our Projects</h5>
-                <p className="card-text  text-white fs-6 w-50 py-2" style={{  opacity: 0.7 }}>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-                <a href="#" className="btn text-white border-white btn-outline-secondary start-0 ">See Projects</a>
-                <img src={img} alt='img' className='position-absolute bottom-0 end-0' style={{ maxWidth: "200px" }} />
+            <motion.div
+              initial={{ opacity: 0, x: 30, y: 50 }}
+              whileInView={{ opacity: 1, x: 5, y: 2 }}
+              transition={{
+                default: {
+                  type: "spring",
+                  damping: 10,
+                  duration: 2,
+                  stiffness: 40,
+                  ease: [0.5, 0.71, 0.2, 1.5],
+                },
+              }}
+            >
+              <div className="card border-opacity-10 text-start bg-primary position-relative">
+                <div className="card-body text-white">
+                  <h5 className="card-title text-white text-start fs-4 fw-semibold">
+                    Our Projects
+                  </h5>
+                  <p
+                    className="card-text  text-white fs-6 w-50 py-2"
+                    style={{ opacity: 0.7 }}
+                  >
+                    Maecenas sed diam eget risus varius blandit sit amet non
+                    magna.
+                  </p>
+                  <a
+                    href="#"
+                    className="btn text-white border-white btn-outline-secondary start-0 "
+                  >
+                    See Projects
+                  </a>
+                  <img
+                    src={img}
+                    alt="img"
+                    className="position-absolute bottom-0 end-0"
+                    style={{ maxWidth: "200px" }}
+                  />
+                </div>
               </div>
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
--- src/components/techno/techno.jsx
+++ src/components/techno/techno.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Techno.css";
 import html from "./icons/html.svg";
@@ -7,40 +7,137 @@ import sass from "./icons/sass.svg";
 import js from "./icons/js.svg";
 import angular from "./icons/angular.svg";
 import python from "./icons/python.svg";
+import { motion } from "framer-motion";
 
 const Techno = () => {
   return (
     <div className="container-fluid bg-primary tech-container mx-auto">
       <div className="container tech mx-auto">
-        <div className='row align-items-center'>
-          <div className='col-lg-6  text-start'>
+        <div className="row align-items-center">
+          <div className="col-lg-6  text-start">
             <h2 className="text-light">We use cutting edge technology</h2>
-            <p className="text-light sidetext" style={{ '--bs-text-opacity': 0.7 }}>
-              Nullam id dolor id nibh ultricies vehicula ut id elit. Aenean lacinia bibendum nulla sed consectetur. Cum sociis
-              natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla vitae elit libero, a pharetra
-              augue. Vestibulum id ligula porta felis euismod semper. Vivamus sagittis lacus vel augue laoreet rutrum faucibus
-              dolor auctor.
+            <p
+              className="text-light sidetext"
+              style={{ "--bs-text-opacity": 0.7 }}
+            >
+              Nullam id dolor id nibh ultricies vehicula ut id elit. Aenean
+              lacinia bibendum nulla sed consectetur. Cum sociis natoque
+              penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+              Nulla vitae elit libero, a pharetra augue. Vestibulum id ligula
+              porta felis euismod semper. Vivamus sagittis lacus vel augue
+              laoreet rutrum faucibus dolor auctor.
             </p>
           </div>
-          <div className='col-lg-6 side-icon'>
-            <div className="row row-cols-1 row-cols-lg-2 g-2 justify-content-center mx-auto " style={{ maxWidth: "200px" }}>
+          <div className="col-lg-6 side-icon">
+            <div
+              className="row row-cols-1 row-cols-lg-2 g-2 justify-content-center mx-auto "
+              style={{ maxWidth: "200px" }}
+            >
               <div className="col py-2  ">
-                <img className='icon' src={html} alt='HTML' />
+                <motion.div
+                  initial={{ opacity: 0, x: -30, y: 50 }}
+                  whileInView={{ opacity: 1, x: 5, y: 2 }}
+                  transition={{
+                    default: {
+                      type: "spring",
+                      damping: 10,
+                      duration: 2,
+                      stiffness: 40,
+                      ease: [0.5, 0.71, 0.2, 1.5],
+                    },
+                  }}
+                >
+                  <img className="icon" src={html} alt="HTML" />
+                </motion.div>
+              </div>
+
+              <div className="col py-2">
+                <motion.div
+                  initial={{ opacity: 0, x: -30, y: -50 }}
+                  whileInView={{ opacity: 1, x: 5, y: 2 }}
+                  transition={{
+                    default: {
+                      type: "spring",
+                      damping: 10,
+                      duration: 2,
+                      stiffness: 40,
+                      ease: [0.5, 0.71, 0.2, 1.5],
+                    },
+                  }}
+                >
+                  <img className="icon" src={css} alt="CSS" />
+                </motion.div>
+              </div>
+
+              <div className="col py-2">
+                <motion.div
+                  initial={{ opacity: 0, x: 80 }}
+                  whileInView={{ opacity: 1, x: 5 }}
+                  transition={{
+                    default: {
+                      type: "spring",
+                      damping: 10,
+                      duration: 2,
+                      stiffness: 40,
+                      ease: [0.5, 0.71, 0.2, 1.5],
+                    },
+                  }}
+                >
+                  <img className="icon" src={sass} alt="Sass" />
+                </motion.div>
+              </div>
+
+              <div className="col py-2">
+                <motion.div
+                  initial={{ opacity: 0, x: -30, y: 50 }}
+                  whileInView={{ opacity: 1, x: 5, y: 2 }}
+                  transition={{
+                    default: {
+                      type: "spring",
+                      damping: 10,
+                      duration: 2,
+                      stiffness: 40,
+                      ease: [0.5, 0.71, 0.2, 1.5],
+                    },
+                  }}
+                >
+                  <img className="icon" src={js} alt="JavaScript" />
+                </motion.div>
+              </div>
+
+              <div className="col py-2">
+                <motion.div
+                  initial={{ opacity: 0, x: -30, y: -50 }}
+                  whileInView={{ opacity: 1, x: 5, y: 2 }}
+                  transition={{
+                    default: {
+                      type: "spring",
+                      damping: 10,
+                      duration: 2,
+                      stiffness: 40,
+                      ease: [0.5, 0.71, 0.2, 1.5],
+                    },
+                  }}
+                >
+                  <img className="icon" src={angular} alt="Angular" />
+                </motion.div>
               </div>
               <div className="col py-2">
-                <img className='icon' src={css} alt='CSS' />
-              </div>
-              <div className="col py-2">
-                <img className='icon' src={sass} alt='Sass' />
-              </div>
-              <div className="col py-2">
-                <img className='icon' src={js} alt='JavaScript' />
-              </div>
-              <div className="col py-2">
-                <img className='icon' src={angular} alt='Angular' />
-              </div>
-              <div className="col py-2">
-                <img className='icon' src={python} alt='Python' />
+                <motion.div
+                  initial={{ opacity: 0, x: 80 }}
+                  whileInView={{ opacity: 1, x: 5 }}
+                  transition={{
+                    default: {
+                      type: "spring",
+                      damping: 10,
+                      duration: 2,
+                      stiffness: 40,
+                      ease: [0.5, 0.71, 0.2, 1.5],
+                    },
+                  }}
+                >
+                  <img className="icon" src={python} alt="Python" />
+                </motion.div>
               </div>
             </div>
           </div>
@@ -51,4 +148,3 @@ const Techno = () => {
 };
 
 export default Techno;
-
