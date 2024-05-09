804dc59267a6beb6c7216299dd3e35de3e4e0a66
Your Namemgit config --global user.email 'mariasol039@hotmail.comgit commit  git config --global user.email you@example.comgit commit -m 'Add new'git pushgit config --global user.email you@example.com  git config --global user.name Your <you@example.com>
2024-05-09 16:49:30 -0300

    Navbar responsive fixed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,36 +1,84 @@
+
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css"; 
 import "./Navbar.css";
-import square from "./square.svg"
+import square from "./square.svg";
 
 export default function Navbar() {
   return (
-    <div className="container  sticky-top navbar-custom m-4 mx-auto">
+    <div className="container sticky-top navbar-custom m-4 mx-auto">
       <nav className="navbar navbar-expand-lg navbar-custom navbar-dark">
         <div className="container">
-          <a className="navbar-brand brand-text" href="#">compleet labs</a> {/* Aplicar clase brand-text para el color del texto de la marca */}
-          <button className="navbar-toggler mx-4 border-0" type="button" data-bs-toggle="collapse" data-bs-theme="dark" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon border-0" ></span>
+          <a className="navbar-brand brand-text" href="#">
+            compleet labs
+          </a>
+          <button
+            className="navbar-toggler mx-4 border-0"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon border-0"></span>
           </button>
-          <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+          <div
+            className="collapse navbar-collapse justify-content-end"
+            id="navbarSupportedContent"
+          >
             <ul className="navbar-nav">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a> {/* Aplicar clase nav-link-text para el color del texto del enlace */}
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" style={{ color: 'var(--third-color)' }} href="#">Services</a> {/* Aplicar clase nav-link-text para el color del texto del enlace */}
+                <a
+                  className="nav-link"
+                  style={{ color: "var(--third-color)" }}
+                  href="#"
+                >
+                  Services
+                </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" style={{ color: 'var(--third-color)' }} href="#">Technology</a> {/* Aplicar clase nav-link-text para el color del texto del enlace */}
+                <a
+                  className="nav-link"
+                  style={{ color: "var(--third-color)" }}
+                  href="#"
+                >
+                  Technology
+                </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" style={{ color: 'var(--third-color)' }} href="#">Case Studies</a> {/* Aplicar clase nav-link-text para el color del texto del enlace */}
+                <a
+                  className="nav-link"
+                  style={{ color: "var(--third-color)" }}
+                  href="#"
+                >
+                  Case Studies
+                </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" style={{ color: 'var(--third-color)' }} href="#">About</a> {/* Aplicar clase nav-link-text para el color del texto del enlace */}
+                <a
+                  className="nav-link"
+                  style={{ color: "var(--third-color)" }}
+                  href="#"
+                >
+                  About
+                </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link hire-us-text" href="#"><img className="mx-1 mb-1" alt="square" src={square}></img>Hire us</a> {/* Aplicar clase hire-us-text para el color del texto de "Hire us" */}
+                <a className="nav-link hire-us-text" href="#">
+                  <img
+                    className="mx-1 mb-1"
+                    alt="square"
+                    src={square}
+                    style={{ maxWidth: "20px" }} // Ajustar tamaño de la imagen para que sea responsive
+                  ></img>
+                  Hire us
+                </a>
               </li>
             </ul>
           </div>
@@ -39,3 +87,4 @@ export default function Navbar() {
     </div>
   );
 }
+
